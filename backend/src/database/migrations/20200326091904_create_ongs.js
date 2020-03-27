f1cd3beedab9f8445025f61d9178e198508f882f
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-03-26 22:05:06 -0300

    Primeira parte do banco

--- backend/src/database/migrations/20200326091904_create_ongs.js
+++ backend/src/database/migrations/20200326091904_create_ongs.js
@@ -1,11 +1,16 @@
 exports.up = function (knex) {
-    knex.schema.createTable('ongs', function (table) {
+    return knex.schema.createTable('ongs', function (table) {
         table.string('id').primary();
-        table.string('nome').notNullable();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+        // table.string('city').notNullable();
     })
 };
 
 exports.down = function (knex) {
-
+    return knex.schema.dropTable('ongs');
 };
 // 999710543 Wilton
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,7 @@
 const {Router} = require('express');
 const routes = Router();
+const OngController = require("./controllers/OngController");
+
 routes.get('/', (request, response) => {
     const params = request.query;
     return response.json({
@@ -7,12 +9,7 @@ routes.get('/', (request, response) => {
         aluno: "Lucas de Oliveira Macedo"
     });
 });
-routes.get('/users', (request, response) => {
-    const params = request.query;
-    return response.json({
-        evento: "Semana OmniStack 1.0",
-        aluno: "Lucas de Oliveira Macedo"
-    });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
