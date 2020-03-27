08a8f44ee0682ee6845b145c44e7eac5c33ff11a
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-03-26 23:24:20 -0300

    Routes da aplicação encerrada

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,9 @@
 const {Router} = require('express');
 const routes = Router();
 const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 routes.get('/', (request, response) => {
     const params = request.query;
@@ -9,7 +12,15 @@ routes.get('/', (request, response) => {
         aluno: "Lucas de Oliveira Macedo"
     });
 });
+routes.post('/sessions', SessionController.create);
+
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
