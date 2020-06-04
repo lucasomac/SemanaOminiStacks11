d01d4ee73ddda6c6a12fcd7232077c36f57aaa32
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-06-04 14:51:51 -0300

    Adicionado pasta WEB

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Routes from "./routes";
+import './global.css';
+
+function App() {
+
+    return (<Routes/>)
+}
+
+export default App;
+
+// const [counter, setCounter] = useState(0);
+//
+// function increment() {
+//     setCounter(counter + 1);
+// }
